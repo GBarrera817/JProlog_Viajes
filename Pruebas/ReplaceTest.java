6ec8abb472b893edbeb0722e251665b919e6276c
Gabriela Barrera <metallica_716@hotmail.com>
2016-11-11 22:49:31 -0300

    README.md actualizado

---
+++ Pruebas/ReplaceTest.java
@@ -0,0 +1,15 @@
+/**
+ * Creado por Gabriela Barrera el 11-11-2016.
+ * Proyecto: JProlog_Viajes
+ */
+public class ReplaceTest {
+
+    public static void main(String[] args) {
+
+        String original =  "HolaHHHHmmmhipis";
+        System.out.println(original);
+        original = original.replace("H","M");
+        System.out.println(original);
+
+    }
+}
